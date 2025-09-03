0ed941e03572ccf6092c3e4638402d994c13c8a8
Felipe Madureira de Souza <Felippefms3@gmail.com>
2025-09-03 08:16:38 -0300

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Inicia a aplicação
-poetry run uvicorn --host 0.0.0.0 --port 8000 feelsocial_api.app:app
+poetry run uvicorn --host 0.0.0.0 --port 8000 feelsocial_api.app:app --reload
--- feelsocial_api/routers/users_routes.py
+++ feelsocial_api/routers/users_routes.py
@@ -5,14 +5,38 @@ from fastapi import APIRouter
 from feelsocial_api.models import User
 from ..schemas.user_schema import UserPublic, UserSchema, UserList
 
-router = APIRouter(prefix='/users', tags=['users'])
+router = APIRouter(tags=['users'])
 
-# @router.get('/', response_model=UserList)
-# def read_users(session: Session, skip: int = 0, limit: int = 100):
-#     users = session.scalars(select(User).offset(skip).limit(limit)).all()
-#     return {'users': users}
+@router.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': UserPublic}
 
 
 @router.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    return user
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Email already exists',
+            )
+
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
